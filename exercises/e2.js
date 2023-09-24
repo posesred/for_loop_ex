b664209deb98a1ce37ab30b5a5616cfb2603a0a8
Sanatjon Burkhanov <sburhanov1977@gmail.com>
2023-09-24 01:41:14 -0400

    init first and second

--- exercises/e2.js
+++ exercises/e2.js
@@ -4,8 +4,27 @@
 
 export function getNumbersWithSquareRoots(max) {
   // Your code goes here...
-
+  let arrOfSquares = []
+  let m =0;
+  for(let i = 0; m<=max;i++ ){
+    if(i*i<max){
+      arrOfSquares.push(i*i)
+    }
+    else{
+      m = i*i;
+    }
+  }
+  return arrOfSquares;
 }
+// I tried to improvise it and chatgpt gave me this 
+// export function getNumbersWithSquareRoots(max) {
+//   // Your code goes here...
+//   let arrOfSquares = [];
+//   for(let i = 0; i * i <= max; i++) {
+//     arrOfSquares.push(i * i);
+//   }
+//   return arrOfSquares;
+// }
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-2"
